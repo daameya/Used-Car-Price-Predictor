26286000190a4a3a2fb6643b020afc12fd20cd82
Ameya Damle <1998ameya@gmail.com>
2023-06-15 17:29:09 +0100

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifacts","train.csv")
-    test_data_path: str=os.path.join("artifacts","test.csv")
-    raw_data_path: str=os.path.join("artifacts","data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -20,10 +24,10 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv("Notebook\Data\cars.csv")
+            df=pd.read_csv('Notebook\Data\cars.csv')
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
@@ -47,4 +51,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/exception.py
+++ src/exception.py
@@ -20,10 +20,3 @@ class CustomException(Exception):
     
 
 
-if __name__=="__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
