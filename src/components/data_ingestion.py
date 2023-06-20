81808bcf71561234a3ea12e6a60f5ec3a6d4f43e
Ameya Damle <1998ameya@gmail.com>
2023-06-20 15:19:16 +0100

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('Notebook\Data\cars.csv')
+            df=pd.read_csv(r'Notebook\Data\cars.csv')
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 #from sklearn.feature_extraction.text import h
 
 from src.exception import CustomException
@@ -17,7 +17,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path: str=os.path.join('artifacts','processor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -30,11 +30,10 @@ class DataTransformation:
         
          """
         try:
-            numerical_columns = [ 'year', 'mileage(kilometers)', 'volume(cm3)']
+            numerical_columns = [ "year", "mileage(kilometers)", "volume(cm3)"]
 
             categorical_columns = [
                 'make',
-                'model',
                 'condition',
                 'fuel_type',
                 'color',
@@ -42,6 +41,35 @@ class DataTransformation:
                 'drive_unit',
                 'segment',
             ]
+
+            #Define the custom ranking for each ordinal variable
+            make_cat = ['mazda', 'mg', 'renault', 'gaz', 'aro', 'rover', 'uaz', 'alfa-romeo', 'audi',
+            'oldsmobile', 'saab', 'peugeot', 'chrysler', 'wartburg', 'moskvich', 'volvo',
+            'fiat', 'roewe', 'porsche', 'zaz', 'luaz', 'dacia', 'lada-vaz', 'izh', 'raf',
+            'bogdan', 'bmw', 'nissan', 'mercedes-benz', 'mitsubishi', 'toyota', 'chery',
+            'gmc', 'hyundai', 'honda', 'ssangyong', 'suzuki', 'opel', 'seat', 'volkswagen',
+            'daihatsu', 'chevrolet', 'geely', 'saturn', 'kia', 'lincoln', 'eksklyuziv',
+            'citroen', 'dong-feng', 'pontiac', 'ford', 'subaru', 'bentley', 'faw',
+            'cadillac', 'lifan', 'plymouth', 'hafei', 'shanghai-maple', 'mini', 'jeep',
+            'skoda', 'mercury', 'changan', 'lexus', 'isuzu', 'aston-martin', 'lancia',
+            'great-wall', 'land-rover', 'jaguar', 'buick', 'daewoo', 'vortex', 'infiniti',
+            'byd', 'smart', 'maserati', 'haval', 'acura', 'scion', 'tata', 'datsun', 'tesla',
+            'mclaren', 'ravon', 'trabant', 'proton', 'fso', 'jac', 'asia', 'iran-khodro',
+            'zotye', 'tagaz', 'saipa', 'brilliance']
+
+            condition_cat = ['with mileage', 'with damage', 'for parts']
+
+            fuel_type_cat = ['petrol', 'diesel', 'electrocar']
+
+            color_cat = ['burgundy', 'black', 'silver', 'white', 'gray', 'blue', 'other', 'purple', 'red',
+            'green', 'brown', 'yellow', 'orange']
+
+            transmission_cat = ['mechanics', 'auto']
+
+            drive_unit_cat = ['front-wheel drive', 'rear drive', 'all-wheel drive',
+            'part-time four-wheel drive']
+
+            segment_cat = ['B', 'C', 'D', 'M', 'E', 'A', 'J', 'S', 'F']
 
             num_pipeline= Pipeline(
                 steps=[
@@ -55,8 +83,7 @@ class DataTransformation:
 
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("string_converter", FunctionTransformer(lambda x: x.astype(str))),
-                ("one_hot_encoder",OneHotEncoder(handle_unknown="ignore")),
+                ("ordinalencoder",OrdinalEncoder(categories=[make_cat,condition_cat,fuel_type_cat,color_cat,transmission_cat,drive_unit_cat,segment_cat])),
                 ("scaler",StandardScaler(with_mean=False))
                 ]
                 
@@ -82,7 +109,7 @@ class DataTransformation:
             raise CustomException(e,sys)
               
     
-    def initiate_data_transformation(self, train_path, test_path):
+    def initiate_data_transformation(self,train_path,test_path):
 
         try:
             train_df=pd.read_csv(train_path)
@@ -94,7 +121,7 @@ class DataTransformation:
             preprocessing_obj=self.get_data_transformer_object()
 
             target_column_name="priceUSD"
-            numerical_columns=['year', 'mileage(kilometers)', 'volume(cm3)']
+            numerical_columns = ["year", "mileage(kilometers)", "volume(cm3)"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -109,9 +136,7 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
 
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
@@ -125,7 +150,7 @@ class DataTransformation:
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
         
         except Exception as e:
