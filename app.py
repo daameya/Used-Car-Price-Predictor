3bf9b20508ab23d2ffcf2cffcdc4dd0ece2f1148
Ameya Damle <1998ameya@gmail.com>
2023-06-28 19:36:23 +0100

    template updated

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app=application
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predictdata', methods=['GET','POST'])
 def predict_datapoint():
